e5454bf724dcd125ac17d26e36d98e32c0964f48
QuentinDuval <senmenty@gmail.com>
2015-10-10 11:07:23 +0200

    More applicative utils

--- src/Utils/Monad.hs
+++ src/Utils/Monad.hs
@@ -8,25 +8,45 @@ import Control.Monad
 
 -- | Helpful combinators to work on monadic values
 
-(.>.), (.<.), (.==.), (./=.) :: (Monad m) => m Double -> m Double -> m Bool
+(.>.), (.<.), (.==.), (./=.), (.<=.), (.>=.) :: (Applicative m) => m Double -> m Double -> m Bool
 (.>.)   = liftA2 (>)
 (.<.)   = liftA2 (<)
 (.==.)  = liftA2 (==)
 (./=.)  = liftA2 (/=)
+(.<=.)  = liftA2 (<=)
+(.>=.)  = liftA2 (>=)
 
-
-(.&&.), (.||.) :: (Monad m) => m Bool -> m Bool -> m Bool
+(.&&.), (.||.) :: (Applicative m) => m Bool -> m Bool -> m Bool
 (.&&.) = liftA2 (&&)
 (.||.) = liftA2 (&&)
 
 
-instance (Monad m, Num a) => Num (m a) where
+instance (Applicative m, Num a) => Num (m a) where
     (+) = liftA2 (+)
     (*) = liftA2 (*)
     (-) = liftA2 (-)
     abs = fmap abs
     signum = fmap signum
-    fromInteger = return . fromInteger
+    fromInteger = pure . fromInteger
+
+instance (Applicative m, Fractional a) => Fractional (m a) where
+    fromRational = pure . fromRational
+    (/) = liftA2 (/)
+
+instance (Applicative m, Floating a) => Floating (m a) where
+    pi  = pure pi
+    exp = fmap exp
+    log = fmap log
+    sin = fmap sin
+    cos = fmap cos
+    asin = fmap asin
+    acos = fmap acos
+    atan = fmap atan
+    sinh = fmap sinh
+    cosh = fmap cosh
+    asinh = fmap asinh
+    acosh = fmap acosh
+    atanh = fmap atanh
 
 
 cst :: (Applicative m, Real a, Fractional b) => a -> m b

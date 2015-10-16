0b8a26bd2c30b6d2fdd60803965d61b2d40eea2c
QuentinDuval <senmenty@gmail.com>
2015-10-16 16:00:51 +0200

    Instances of Result

--- src/EvalProd.hs
+++ src/EvalProd.hs
@@ -18,11 +18,20 @@ import Utils.Time
 data Result a
     = Done a
     | Fail String
-    deriving (Show)
 
 instance Functor Result where
     fmap f (Done a) = Done (f a)
     fmap f (Fail s) = Fail s
+
+instance Applicative Result where
+    pure         = Done
+    Done f <*> a = fmap f a
+    Fail s <*> _ = Fail s
+
+instance Monad Result where
+    return       = pure
+    Done a >>= f = f a
+    Fail s >>= _ = Fail s
 
 
 -- | Environment of evaluation of financial product

3c77a3b66cd5b99d012d59fb0cccd5d823866908
QuentinDuval <senmenty@gmail.com>
2015-11-08 20:25:28 +0100

    Refactoring

--- src/Listed/Option/BestOf.hs
+++ src/Listed/Option/BestOf.hs
@@ -1,11 +1,12 @@
 {-# LANGUAGE RecordWildCards #-}
 module Listed.Option.BestOf (
-    module Listed.Option.European,
+    module Listed.Option.Core,
     bestOfOption,
 ) where
 
 import Data.Monoid((<>))
-import Listed.Option.European
+import Eval
+import Listed.Option.Core
 import Observable
 import Payoff
 import Utils.Monad
@@ -14,13 +15,5 @@ import Utils.Time
 
 bestOfOption :: CompositeOption -> FinDate -> FinProduct
 bestOfOption (CompositeOption OptionHeader{..} bodies) t1
-    = premium <> allOf (fmap (simpleOptionBody t1) bodies)
---    = premium <> opt
---    where
---        t2  = t1 `addDay` maturity
---        val = stock buyInstr t2 / stock sellInstr t2
---        opt = ifThen (val .>. cst strike) $
---                scale quantity $ mconcat [
---                    send $ trn 1      t2 buyInstr,
---                    give $ trn strike t2 sellInstr]
+    = premium <> bestOf (fmap (simpleOptionBody t1) bodies) `withEvalOn` (Stock "USD", t1)
 

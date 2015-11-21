771c8841ccfd4ae4fc234b6991aec38d872f08c1
QuentinDuval <senmenty@gmail.com>
2015-11-21 18:32:47 +0100

    Enhancing the predicates

--- src/Observable/PredicateUtils.hs
+++ src/Observable/PredicateUtils.hs
@@ -1,7 +1,9 @@
 module Observable.PredicateUtils (
     findFirst,
+    filterIf,
 ) where
 
+import Control.Monad
 import Eval
 import Observable.Class
 import Observable.Predicate
@@ -15,5 +17,14 @@ findFirst cs ps = do
     let conditions = fmap evalObs cs
     firstMatch <- findM fst (zip conditions ps)
     pure (fmap snd firstMatch)
+
+
+-- Algorithm to find all elements matching their respective predicate
+
+filterIf :: (Monad m) => [ObsPredicate] -> [p] -> EvalProd m [p]
+filterIf cs ps = do
+    let conditions = fmap evalObs cs
+    matches <- filterM fst (zip conditions ps)
+    pure (fmap snd matches)
 
 
